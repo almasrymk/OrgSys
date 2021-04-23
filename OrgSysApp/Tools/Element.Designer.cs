dee394f666a7f4ad9b604bbf1e0eb64b892b777d
mohammed khaled <almasrymk@gmail.com>
2021-04-23 19:22:46 +0200

    edit

---
+++ OrgSysApp/Tools/Element.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace OrgSysApp.Tools
+{
+    partial class Element
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgImage = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgImage
+            // 
+            this.imgImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgImage.Location = new System.Drawing.Point(0, 0);
+            this.imgImage.Name = "imgImage";
+            this.imgImage.Size = new System.Drawing.Size(100, 95);
+            this.imgImage.TabIndex = 4;
+            this.imgImage.TabStop = false;
+            this.imgImage.Click += new System.EventHandler(this.this_Click);
+            this.imgImage.MouseEnter += new System.EventHandler(this.this_MouseEnter);
+            this.imgImage.MouseLeave += new System.EventHandler(this.this_MouseLeave);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblTitle.Location = new System.Drawing.Point(0, 95);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(100, 30);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Title";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.Click += new System.EventHandler(this.this_Click);
+            this.lblTitle.MouseEnter += new System.EventHandler(this.this_MouseEnter);
+            this.lblTitle.MouseLeave += new System.EventHandler(this.this_MouseLeave);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblDescription.Location = new System.Drawing.Point(0, 125);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(100, 25);
+            this.lblDescription.TabIndex = 3;
+            this.lblDescription.Click += new System.EventHandler(this.this_Click);
+            this.lblDescription.MouseEnter += new System.EventHandler(this.this_MouseEnter);
+            this.lblDescription.MouseLeave += new System.EventHandler(this.this_MouseLeave);
+            // 
+            // Element
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.imgImage);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblDescription);
+            this.Name = "Element";
+            this.Size = new System.Drawing.Size(100, 150);
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgImage;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
---
+++ OrgSysApp/Tools/Element.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OrgSysApp.Tools
+{
+    public partial class Element : UserControl
+    {
+        public Element()
+        {
+            InitializeComponent();
+        }
+
+        [Category("Org")]
+        public event EventHandler click;
+
+        Image _image = null;
+        Image _imageActive = null;
+        ImageLayout _imageLayOut = ImageLayout.Zoom;
+        string _title = "";
+        string _description = "";
+        Font _font = null;
+        Font _fontDescription = null;
+        Color _fontColor = Color.Black;
+        Color _fontColorActive = Color.Black;
+        Color _fontColorDescription = Color.Black;
+        Color _fontColorActiveDescription = Color.Black;        
+        bool _active = false;
+
+        [Category("Org.Image")]
+        public Image image
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                imgImage.BackgroundImage = _image;
+            }
+        }
+
+        [Category("Org.Image")]
+        public Image imageActive
+        {
+            get { return _imageActive; }
+            set { _imageActive = value; }
+        }
+
+        [Category("Org.Image")]
+        public ImageLayout layOut
+        {
+            get { return _imageLayOut; }
+            set
+            {
+                _imageLayOut = value;
+                imgImage.BackgroundImageLayout = value;
+            }
+        }
+
+        [Category("Org.Title")]
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                lblTitle.Text = value;
+            }
+        }
+
+        [Category("Org.Title")]
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                lblDescription.Text = value;
+            }
+        }
+        
+        [Category("Org")]
+        public string ButtonName { get; set; }
+
+        [Category("Org.Title")]
+        public Font font
+        {
+            get { return _font; }
+            set
+            {
+                _font = value;
+                lblTitle.Font = value;
+            }
+        }
+
+        [Category("Org.Title")]
+        public Color fontColor
+        {
+            get { return _fontColor; }
+            set
+            {
+                _fontColor = value;
+                lblTitle.ForeColor = _fontColor;
+            }
+        }
+
+        [Category("Org.Title")]
+        public Color fontColorActive
+        {
+            get { return _fontColorActive; }
+            set { _fontColorActive = value; }
+        }
+
+        [Category("Org.Title")]
+        public Font fontDescription
+        {
+            get { return _fontDescription; }
+            set
+            {
+                _fontDescription = value;
+                lblDescription.Font = value;
+            }
+        }
+
+        [Category("Org.Title")]
+        public Color fontColorDescription
+        {
+            get { return _fontColorDescription; }
+            set
+            {
+                _fontColorDescription = value;
+                lblDescription.ForeColor = _fontColor;
+            }
+        }
+
+        [Category("Org.Title")]
+        public Color fontColorActiveDescription
+        {
+            get { return _fontColorActiveDescription; }
+            set { _fontColorActiveDescription = value; }
+        }
+
+        [Category("Org")]
+        public bool active
+        {
+            get { return _active; }
+            set
+            {
+                _active = value;
+                if (_active)
+                {
+                    imgImage.BackgroundImage = _imageActive;
+                    lblTitle.ForeColor = _fontColorActive;
+                    lblDescription.ForeColor = _fontColorActiveDescription;
+                }
+                else
+                {
+                    imgImage.BackgroundImage = _image;
+                    lblTitle.ForeColor = _fontColor;
+                    lblDescription.ForeColor = _fontColorDescription;
+                }
+            }
+        }
+
+        private void this_MouseEnter(object sender, EventArgs e)
+        {
+            imgImage.BackgroundImage = _imageActive;
+            lblTitle.ForeColor = _fontColorActive;
+            lblDescription.ForeColor = _fontColorActiveDescription;
+        }
+
+        private void this_MouseLeave(object sender, EventArgs e)
+        {
+            if (!_active)
+            {
+                imgImage.BackgroundImage = _image;
+                lblTitle.ForeColor = _fontColor;
+                lblDescription.ForeColor = _fontColorDescription;
+            }
+        }
+
+        private void this_Click(object sender, EventArgs e)
+        {
+            if (click != null)
+                click(this, e);
+        }
+    }
+}
