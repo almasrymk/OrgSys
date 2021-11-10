7014cfc5a125f2bfbe6ccd55e62a31d4d4254762
mohamed abdelkhalek <memo_mad2007@hotmail.com>
2021-11-10 21:25:15 +0200

    Report

--- Entity/BaseModel.cs
+++ Entity/BaseModel.cs
@@ -24,6 +24,7 @@ namespace Entity
 
         public string ImgPath { get; set; }      
 
-        public Status Status { get; set; }        
+        public Status Status { get; set; }
+        
     }
 }
