cccec2fcefd090256e9fe7a94e9426dd8618cb06
mazidan77 <moga4arab@gmail.com>
2024-04-07 14:54:22 +0200

    add account

--- OrgSys/Areas/Setting/Controllers/AccountController.cs
+++ OrgSys/Areas/Setting/Controllers/AccountController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entity.ModelView;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OrgSys.Controllers;
+using Service;
+using System.Linq;
 
 namespace OrgSys.Areas.Setting.Controllers
 {
-    public class AccountController : Controller
+    [Area("Setting")]
+    public class AccountController : BaseController<AccountModelView>
     {
-        public IActionResult Index()
+        public override void LoadViewBag(AccountModelView model)
         {
-            return View();
+            ViewBag.BranchList = new SelectList(new AccountService(User.GetSchema()).GetAll(model.ParentId, model.TypeId), "Id", "Name", model.ParentId);
+            ViewBag.AccountType = new SelectList(new AccountTypeService(User.GetSchema()).GetAll(model.ParentId, model.TypeId), "Id", "Name", model.AccountTypeId);
+
+        }
+
+        public JsonResult GetList(string txtSearch = "", int page = 1, int pageSize = 10)
+        {
+            if (txtSearch != null)
+                txtSearch = txtSearch.Trim().ToLower();
+
+            var itemsList = new AccountService(User.GetSchema()).GetAll(txtSearch, 0, 0, page, pageSize);
+            var list = itemsList.Distinct().OrderBy(_ => _.Name)
+                .Select(_ => new
+                {
+                    _.Id,
+                    _.Name
+                })
+                .ToList();
+            return Json(list);
         }
     }
 }
