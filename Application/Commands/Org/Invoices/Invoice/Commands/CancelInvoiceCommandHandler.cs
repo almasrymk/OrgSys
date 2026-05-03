86de984193139a0d39985bcfcdffd2ba527fa2aa
mohammed khaled <almasrymk@gmail.com>
2026-05-03 08:31:10 +0300

    fix

--- Application/Commands/Org/Invoices/Invoice/Commands/CancelInvoiceCommandHandler.cs
+++ Application/Commands/Org/Invoices/Invoice/Commands/CancelInvoiceCommandHandler.cs
@@ -13,23 +13,20 @@ using System.Text;
 
 namespace Application.Commands.Org.Invoices.Invoice.Commands
 {
-    public record CancelInvoiceCommand(long Id) : ICommand , ICancelCommand;
+    public record CancelInvoiceCommand(long Id) : ICommand, IUpdateCommand<Result>;
 
-    public class CancelInvoiceCommandHandler(IUnitOfWork unitOfWork,IRepository<Entity.Model.Invoice> repository , IMapper mapper, IServiceProvider provider) : UpdateCommandHandler<CancelInvoiceCommand, Entity.Model.Invoice>(unitOfWork, repository, mapper, provider)
+    public class CancelInvoiceCommandHandler(IUnitOfWork _UnitOfWork, IRepository<Entity.Model.Invoice> _Repository, IMapper mapper, IServiceProvider _provider) : UpdateCommandHandler<UpdateInvoiceCommand, Entity.Model.Invoice>(_UnitOfWork, _Repository, mapper, _provider)
     {
-        private readonly IUnitOfWork _unitOfWork = unitOfWork;
-        private readonly IRepository<Entity.Model.Invoice> _repository = repository;
 
-        public override async Task<Result> Handle(CancelInvoiceCommand request, CancellationToken ct)
+        public override async Task<Result> Handle(UpdateInvoiceCommand request, CancellationToken cancellationToken)
         {
-
             try
             {
-                var invoice = await _repository.GetByFilterAsync(x => x.Id == request.Id, await CreateInclude());
+                var invoice = await _Repository.GetByFilterAsync(x => x.Id == request.Id, await CreateInclude());
                 if (invoice is null)
                     return new Result(HttpStatusCode.NotFound, new List<Error> { new Error("Invoice not found") });
                 invoice.Status = Utility.Status.Cancel;
-                if (await _unitOfWork.SaveChangeAsync() > 0)
+                if (await _UnitOfWork.SaveChangeAsync() > 0)
                 {
                     return new Result(HttpStatusCode.OK, null);
                 }
@@ -41,7 +38,7 @@ namespace Application.Commands.Org.Invoices.Invoice.Commands
                 return new Result(
                   HttpStatusCode.InternalServerError,
                   new List<Error> { new Error(ex.Message) });
-            } 
-        }
+            }
+        }       
     }
 }
