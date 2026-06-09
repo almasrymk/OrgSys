d903783cf754194f1743b1e4bef518944601c6aa
mohammed khaled <almasrymk@gmail.com>
2026-06-09 13:45:24 +0300

    Add Account to Safe and Dealers

--- OrgSys/Areas/Setting/Controllers/SafeController.cs
+++ OrgSys/Areas/Setting/Controllers/SafeController.cs
@@ -4,6 +4,7 @@
     using AutoMapper;
     using Entity.ModelView;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.Extensions.Configuration;
     using OrgSys.Controllers;
     using Service;
@@ -13,7 +14,28 @@
 
     [Area("Setting")]
     public class SafeController(IConfiguration configuration, IMapper mapper) : MainController<SafeModelView, CreateSafeCommand, UpdateSafeCommand>(configuration, mapper)
-    {       
+    {
+        public override async Task LoadViewBag(SafeModelView model)
+        {
+            ViewBag.AccountList = new SelectList(await GetListApi<AccountModelView>(TypeId: model.TypeId, Page: 1, PageSize: 20), "Id", "Name", model.AccountId);
+        }
+
+        public override async Task<SafeModelView> InitializeData(SafeModelView ob)
+        {
+            ViewBag.AccountList = new SelectList(await GetListApi<AccountModelView>(TypeId: ob.TypeId, Page: 1, PageSize: 20), "Id", "Name", ob.AccountId);
+
+            if (ob == null)
+                ob = new SafeModelView();
+            if (ob.Id == 0)
+            {
+                ob.CodeNumber = long.Parse("0" + await GetValueApi<SafeModelView>($"GetMax?TypeId={ob.TypeId}")) + 1;
+                ob.Code = "" + ob.CodeNumber;
+            }
+            ob.AccountName = (await GetObApi<AccountModelView>($"GetById?Id={ob.AccountId ?? 0}"))?.Name;
+
+            return ob;
+        }
+
         public async Task<JsonResult> GetList(string txtSearch = "", int page = 1, int pageSize = 20)
         {
             if (txtSearch != null)
--- Repository/Migrations/OrgMigrations/20260609090055_AddAccountToSafeTb.Designer.cs
+++ Repository/Migrations/OrgMigrations/20260609090055_AddAccountToSafeTb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -11,9 +12,11 @@ using Repository;
 namespace Repository.Migrations.OrgMigrations
 {
     [DbContext(typeof(OrgContext))]
-    partial class OrgContextModelSnapshot : ModelSnapshot
+    [Migration("20260609090055_AddAccountToSafeTb")]
+    partial class AddAccountToSafeTb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
